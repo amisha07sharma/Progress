ab5fc6c5516e9d71bf3e55e9785908e032962b73
Amisha <msg.amisha@gmail.com>
2022-01-19 17:14:24 +0530

    [Amisha | Created a channel ]

--- main.go
+++ main.go
@@ -5,15 +5,38 @@ import (
 	"time"
 )
 
-func main() {
-	go func() {
-		for i := 0; i < 5; i++ {
-			fmt.Println("Anonymous goroutine")
-		}
-	}()
-
-	go fmt.Println("Goroutine")
-	time.Sleep(time.Second)
-	fmt.Println("Normal")
-
+func printText() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("Anonymous goroutine")
+	}
 }
+
+func main() {
+
+	// ping = recieving
+	// pong = sending
+	ch := make(chan int32, 8)
+
+	time.Sleep(time.Second * 5)
+
+	ch <- 3
+	go fmt.Println("ping")
+	time.Sleep(time.Second * 5)
+
+	i := <-ch
+
+	go fmt.Println(i, "pong")
+
+	go printText()
+
+	time.Sleep(time.Second * 5)
+
+	fmt.Println("Normal")
+}
+
+// 5 rounds
+// ping pong
+// once ping then pong
+// what is functional programmimg, how gunctional programming can be achieved using golang.
+// currying
+// dining philosophers problem
