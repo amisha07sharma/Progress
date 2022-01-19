b2f9e3a0c2cfa7583aabed9c08458bf2ec227082
Amisha <msg.amisha@gmail.com>
2022-01-19 13:24:12 +0530

    [Amisha | Created anonymous goroutine]

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"time"
 )
 
-//go routines
-
 func main() {
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println("Anonymous goroutine")
+		}
+	}()
+
 	go fmt.Println("Goroutine")
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second)
 	fmt.Println("Normal")
+
 }
