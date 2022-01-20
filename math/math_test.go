221fce1ec66998ea45c4e8b506ef7408fcbd4bfc
Amisha <msg.amisha@gmail.com>
2022-01-20 23:14:46 +0530

    [Amisha | Added a Table Driven test for Add method]

--- math/math_test.go
+++ math/math_test.go
@@ -19,3 +19,25 @@ func TestSubtraction(t *testing.T) {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
 }
+
+// Table Drivem tests
+
+type addTest struct {
+	arg1, arg2, expected int
+}
+
+var addTests = []addTest{
+	addTest{1, 2, 3},
+	addTest{2, 3, 5},
+	addTest{8, 4, 12},
+	addTest{3, 10, 13},
+}
+
+func AddTestTable(t *testing.T) {
+	for _, test := range addTests {
+		output := Add(test.arg1, test.arg2)
+		if output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
+}
