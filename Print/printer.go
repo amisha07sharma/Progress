cfd59a9b8776c8e0720d8cf885fe58e915e0cd4f
Amisha <msg.amisha@gmail.com>
2022-01-22 00:20:55 +0530

    [Amisha | Added general purpose interface]

--- Print/printer.go
+++ Print/printer.go
@@ -3,8 +3,13 @@ package Printer
 import (
 	"bytes"
 	"fmt"
+	"io"
 )
 
 func printer(writer *bytes.Buffer, str string) {
+	fmt.Fprintf(writer, "Hello, %s", str)
+}
+
+func printer2(writer io.Writer, str string) {
 	fmt.Fprintf(writer, "Hello, %s", str)
 }
--- Print/printer_test.go
+++ Print/printer_test.go
@@ -16,3 +16,15 @@ func TestPrinter(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestPrinter2(t *testing.T) {
+	buffer := bytes.Buffer{}
+	printer2(&buffer, "Amisha")
+	got := buffer.String()
+	want := "Hello, Amisha"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
