9092826fe7f9d0065e694874771663aa1232c381
Amisha <msg.amisha@gmail.com>
2022-01-22 21:28:07 +0530

    Amisha | Added time.sleep to add time between 2 print statements

--- MockPractise/countdown.go
+++ MockPractise/countdown.go
@@ -3,6 +3,7 @@ package count
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -10,7 +11,10 @@ const countdownStart = 3
 
 func countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 }
